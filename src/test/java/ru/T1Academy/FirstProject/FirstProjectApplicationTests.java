1bec02df5e4229cacd0ed5068185772dd21b22e2
lexa1480 <lexa.f14@gmail.com>
2025-03-26 19:04:50 +0300

    Init SpringProject

---
+++ src/test/java/ru/T1Academy/FirstProject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.T1Academy.FirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
